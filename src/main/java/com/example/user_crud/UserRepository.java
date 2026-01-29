1fa1a5ce129df85aebb90e157fc802dad594d51d
simomanno <s.mannini1@studenti.unipi.it>
2026-01-29 18:20:42 +0100

    removed reduntant save

--- src/main/java/com/example/user_crud/UserRepository.java
+++ src/main/java/com/example/user_crud/UserRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User save(User user);
-
     Optional<User> findById(long id);
 
     List<User> findByNameAndSurname(String name, String surname);
